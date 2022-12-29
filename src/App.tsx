69cfb82ea6881538413bc5327ac2b4d42e67a534
kohki-nakamura <42920380+kohki-nakamura@users.noreply.github.com>
2022-12-30 00:44:25 +0900

    作成ボタン押下時にtodoを配列に格納

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  type Todo = {
+    inputValue: string;
+    id: number;
+    checked: boolean;
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!inputValue) return;
+
+    const newTodo: Todo = {
+      inputValue: inputValue,
+      id: inputValue.length,
+      checked: false,
+    }
+    setTodos([...todos, newTodo]);
+
+    setInputValue("");
+  };
+
   return (
     <div className="App">
       <div>
         <h2>Todoリスト with Typescript</h2>
-        <form onSubmit={() => {}}>
-          <input type="text" onChange={() => {}} className="inputText" />
+        <form onSubmit={(e) => {handleSubmit(e)}}>
+          <input type="text" onChange={(e) => {handleChange(e)}} className="inputText" value={inputValue} />
           <input type="submit" value="作成" className='submitButton' />
         </form>
       </div>
