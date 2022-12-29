00ba61fb48bc17d584c46a6bf4293ea018ecca88
kohki-nakamura <42920380+kohki-nakamura@users.noreply.github.com>
2022-12-30 01:14:14 +0900

    タスク削除機能

--- src/App.tsx
+++ src/App.tsx
@@ -52,6 +52,11 @@ function App() {
     setTodos(newTodos);
   };
 
+  const handleDelete = (id: number) => {
+    const newTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodos);
+  }
+
   return (
     <div className="App">
       <div>
@@ -66,6 +71,7 @@ function App() {
             <li key={todo.id}>
               <input type="text" onChange={(e) => {handleEdit(todo.id, e.target.value)}} className="inputText" value={todo.inputValue} disabled={todo.checked} />
               <input type="checkbox" onChange={() => {handleChecked(todo.id, todo.checked)}} />
+              <button onClick={() => handleDelete(todo.id)}>消</button>
             </li>
           ))}
         </ul>
